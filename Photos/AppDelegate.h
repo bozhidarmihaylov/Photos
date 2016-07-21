65a9e4e0dfe8acd9a902d9ad3510a53a0df31417
Bozhidar Mihaylov <bozhidar.mihaylov@gmail.com>
2016-07-21 03:01:29 -0700

    Created Xcode project.

---
+++ Photos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photos
+//
+//  Created by user119008 on 7/21/16.
+//  Copyright © 2016 Bozhidar Mihaylov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Photos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Photos
+//
+//  Created by user119008 on 7/21/16.
+//  Copyright © 2016 Bozhidar Mihaylov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
